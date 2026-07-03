e759eb9216c0f69d7b3bb4c0a7ad2ba518878e86
flushy <nazimovroman99@gmail.com>
2026-07-03 20:24:56 +0500

    I DONT KNOW WHATS HAPPENING

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev
 export default defineConfig({
-  plugins: [
-  react(),
-  tailwindcss(),
-  ],
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: '.' // Keeps the build files at the root for GitHub Pages
+  }
 })
